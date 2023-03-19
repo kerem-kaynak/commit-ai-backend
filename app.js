dccc42f3a7d403c3c9137c21bb2f1f9edccd417b
keremakaynak <kerem.kaynak@code.berlin>
2023-03-19 23:28:22 +0100

    feat: backend auth middleware with firebase

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+const { checkIfAuthenticated } = require('./helpers/auth')
+const cors = require('@fastify/cors')
+
 const fastify = require('fastify')({
 	logger: true,
 	prettyPrint: true,
 	disableRequestLogging: false
 })
 
+const registerCors = async () => {await fastify.register(cors, { 
+	// put your options here
+	origin: true,
+	methods: ['GET']
+})}
+
+registerCors()
+
 fastify.get('/', async (req, res) => { res.send({ test: 'hello2' }) })
+
+fastify.route({
+	method: 'GET',
+	url: '/authed',
+	preHandler: checkIfAuthenticated,
+	handler: async (req, res) => {
+		res.send('Success!')
+	}
+})
 
 const PORT = parseInt(parseInt(process.env.PORT)) || 8080
 const HOST = parseInt(parseInt(process.env.HOST)) || '0.0.0.0'
